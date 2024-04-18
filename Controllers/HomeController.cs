a61f0f46f03dad2ce80ec988cb8613d09494eb7f
gabriel cuadros <gabrielcuadros@gmail.com>
2024-04-18 09:41:16 -0500

    fix

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
@@ -9,13 +10,24 @@ namespace SimpleFrameworkApp.Controllers
 {
     public class HomeController : Controller
     {
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+
         public ActionResult Index()
         {
+            _logger.LogInformation("Página de inicio visitada.");
             return View();
         }
 
         public ActionResult About()
         {
+            _logger.LogInformation("Página de about visitada.");
             ViewBag.Message = "Your application description page.";
 
             return View();
@@ -23,6 +35,7 @@ namespace SimpleFrameworkApp.Controllers
 
         public ActionResult Contact()
         {
+            _logger.LogInformation("Página de contact visitada.");
             var demotext = ConfigurationManager.AppSettings["demokeyvalue"];
             ViewBag.Message = "Your contact page."+ demotext;
 
