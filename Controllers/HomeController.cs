2a1dc1078ee630b000b9554a1592a9b11acd8b01
gabriel cuadros <gabrielcuadros@gmail.com>
2024-04-18 11:59:44 -0500

    fix

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,32 +5,26 @@ using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebGrease;
+using log4net;
+using LogManager = log4net.LogManager;
 
 namespace SimpleFrameworkApp.Controllers
 {
     public class HomeController : Controller
     {
-
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController()
-        { }
-        
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        private static readonly ILog _log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
 
         public ActionResult Index()
         {
-            _logger.LogInformation("Página de inicio visitada.");
+            _log.Info("Página de inicio visitada.");
             return View();
         }
 
         public ActionResult About()
         {
-            _logger.LogInformation("Página de about visitada.");
+            _log.Info("Página de about visitada.");
             ViewBag.Message = "Your application description page.";
 
             return View();
@@ -38,7 +32,7 @@ namespace SimpleFrameworkApp.Controllers
 
         public ActionResult Contact()
         {
-            _logger.LogInformation("Página de contact visitada.");
+            _log.Info("Página de contact visitada.");
             var demotext = ConfigurationManager.AppSettings["demokeyvalue"];
             ViewBag.Message = "Your contact page."+ demotext;
 
